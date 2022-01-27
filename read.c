7c3fdc84e2b23b9c050adfceca3b480bfae0ca04
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2022-01-27 15:59:53 -0500

    Create read.c

---
+++ read.c
@@ -0,0 +1,36 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    int dat;
+    int L[] = {1,2,3,4,5};
+    int freep = 0;
+    
+    
+    fp = fopen("map", "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        if (strcmp(read, "Sector Define") == 0)
+        {
+            printf("SECTOR DEFINED");
+        }
+        if (dat == 1)
+        {
+            L[freep] = read;
+            freep += 1;
+        }
+    }
+
+    fclose(fp);
+    if (line)
+        free(line);
+    exit(EXIT_SUCCESS);
+}
