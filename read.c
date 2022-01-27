e26c78584a4673815f9acc5d9acbd0844d6ab0f1
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2022-01-27 16:14:50 -0500

    Update read.c

--- read.c
+++ read.c
@@ -9,7 +9,8 @@ int main(void)
     size_t len = 0;
     ssize_t read;
     int dat;
-    int L[] = {1,2,3,4,5};
+    int L[] = {1,2};
+    int Mod[] = {1,2};
     int freep = 0;
     
     
@@ -33,4 +34,30 @@ int main(void)
     if (line)
         free(line);
     exit(EXIT_SUCCESS);
+    
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    int dat;
+    int freep = 0;
+    
+    
+    fp = fopen("map", "r");
+    if (fp == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        Mod[freep] = read;
+        freep += 1;
+    }
+
+    fclose(fp);
+    if (line)
+        free(line);
+    exit(EXIT_SUCCESS);
+    
+    
 }
